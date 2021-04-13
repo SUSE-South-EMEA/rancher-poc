5a9b5746c33ceef5e828182d363fd3c34c876b7c
root <benoit.loriot@suse.com>
2021-04-13 15:52:55 +0200

    Fix sudo pour installer binaire helm

--- 02-rke_deploy.sh
+++ 02-rke_deploy.sh
@@ -99,7 +99,7 @@ DESC_HELM_INSTALL="Installation de HELM? \n Helm Version: ${HELM_VERSION}${bold}
 COMMAND_HELM_INSTALL() {
 curl -O https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
 tar zxvf helm-v${HELM_VERSION}-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin/helm
+sudo mv linux-amd64/helm /usr/local/bin/helm
 rm -rf linux-amd64/
 rm helm-v${HELM_VERSION}-linux-amd64.tar.gz
 }
