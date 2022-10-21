4e7608e018ce71068cd683fcde03ab5451c98af6
Benoit Loriot <benoit.loriot@gmail.com>
2022-10-21 11:10:22 +0200

    Update 04-cleanup-destroy.sh
    Force cleanup of rancher and rke2 directories.
    rke2-uninstall script  does not always cleanup everything.

--- 04-cleanup-destroy.sh
+++ 04-cleanup-destroy.sh
@@ -10,8 +10,17 @@ for h in ${HOSTS[*]}; do
 echo  
 echo "${bold}${TXT_NODE_UNINSTALL:=Uninstall rke2 on node} $h${normal}" 
 echo
-ssh $h "sudo /usr/local/bin/rke2-killall.sh"
-ssh $h "sudo /usr/local/bin/rke2-uninstall.sh ; sudo rm -rf /etc/rancher"
+ssh $h "sudo /usr/local/bin/rke2-killall.sh ;
+sudo /usr/local/bin/rke2-uninstall.sh ;
+sudo systemctl stop rancher-system-agent.service ; 
+sudo systemctl disable rancher-system-agent.service ;
+sudo rm -f /etc/systemd/system/rancher-system-agent.service ;
+sudo rm -f /etc/systemd/system/rancher-system-agent.env ;
+sudo systemctl daemon-reload ;
+sudo rm -f /usr/local/bin/rancher-system-agent ;
+sudo rm -rf /etc/rancher/ ;
+sudo rm -rf /var/lib/rancher/ ;
+sudo rm -rf /usr/local/bin/rke2*"
 done
 }
 
